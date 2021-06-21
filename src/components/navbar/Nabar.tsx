78904cefd32a112ba45aa32102d085abb46d3931
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-06-21 16:32:16 +0530

    feature-mobile searchbar added

--- src/components/navbar/Nabar.tsx
+++ src/components/navbar/Nabar.tsx
@@ -2,17 +2,37 @@ import classes from "./Navbar.module.css";
 import { Avatar } from "./avatar/Avatar";
 import { useHistory } from "react-router-dom";
 import { SearchBar } from "./searchBar/SearchBar";
+import { Search, Cancel } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
+import { useState } from "react";
 
 export const Navbar = () => {
   const { push } = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className={classes["navbar-container"]}>
-      <h1 className={classes["navbar-logo"]} onClick={() => push("/")}>
-        SociaLink
-      </h1>
-      <div className={classes["requests-avatar-container"]}>
-        <SearchBar />
+      <div className={classes["grouping-container"]}>
+        {showSearch ? (
+          <div className={classes["mobile-searchbar"]}>
+            <SearchBar />
+          </div>
+        ) : (
+          <h1 className={classes["navbar-logo"]} onClick={() => push("/")}>
+            SociaLink
+          </h1>
+        )}
+        <IconButton
+          onClick={() => setShowSearch((state) => !state)}
+          className={classes["mobile-search-button"]}
+        >
+          {showSearch ? <Cancel /> : <Search />}
+        </IconButton>
+      </div>
+      <div className={classes["grouping-container"]}>
+        <div className={classes["desktop-searchbar"]}>
+          <SearchBar />
+        </div>
         <Avatar />
       </div>
     </div>
